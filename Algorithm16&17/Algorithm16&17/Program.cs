976a7113c878d27d51096fad0a52e5d4dd730dc3
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-03-24 22:37:07 -0400

    [Ruchita] Add: Implementation of bubble sort

--- Algorithm16&17/Algorithm16&17/Program.cs
+++ Algorithm16&17/Algorithm16&17/Program.cs
@@ -1,29 +1,54 @@
-﻿namespace Algorithm16_17
+﻿using System.Runtime.InteropServices;
+
+namespace Algorithm16_17
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        //Bubble sort program
+        //Function to print Array
+        public static void print(int[] arr)
         {
-            //Print numbers between 0 to 1000 range 
-            Console.WriteLine("Prime numbers between 0 to 1000");
-            for (int number = 1; number <= 1000; number++)
+            //It will print array lenghth
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
             {
-                int count = 0;
-                for (int i = 1; i <= number; i++)
+                Console.WriteLine(arr[i]);
+            }
+        }
+        //Implementation of a bubble sort
+        public static void bubble(int[] a)
+        {
+            //Length of array
+            int n = a.Length;
+            int i, j, temp;
+            for (i = 0; i < n; i++)
+            {
+                for (j = i + 1; j < n; j++)
                 {
-                    if (number % i == 0)
+                    if (a[j] < a[i])
                     {
-                        count++;
+                        temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
                     }
                 }
-               
-                if (count == 2)
-                {
-                    Console.WriteLine(number);
-                }
-                
             }
-            Console.ReadLine();
+
+        }
+           public static void Main(string[] args)
+            {
+                //Array Declaration 
+             int[] a = { 15,100,25,8,38,17 };
+                Console.WriteLine("Before sorting array elements are:");
+            print(a);
+                //Method to call sorted array
+
+                bubble(a);
+                Console.WriteLine("\n After sorting array elements are:-");
+                print(a);
+
+            }
         }
     }
-    }
+
